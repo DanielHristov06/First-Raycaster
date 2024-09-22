1cc988ab413825d167da974f26b3a1a3ed167450
TheEliteD <dhristov06@gmail.com>
2024-09-22 16:33:13 +0300

    raycasting

--- Classes/ray.h
+++ Classes/ray.h
@@ -12,6 +12,10 @@ class MyRay{
             if (angle < 0) angle = (2 * M_PI) + angle;
             return angle;
         }
+
+        float distanceBetween(float x1, float y1, float x2, float y2){
+            return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+        }
         
     public:
         Player& rayPlayer;
@@ -23,14 +27,15 @@ class MyRay{
         bool isFacingRight = rayAngle < 0.5 * M_PI || rayAngle > 1.5 * M_PI;
         bool isFacingLeft = !isFacingRight;
 
-        int wallHitX = 0;
-        int wallHitY = 0;
+        float wallHitX = 0;
+        float wallHitY = 0;
 
         MyRay(Player& player, double angle, Map map) : rayPlayer(player), rayAngle(normalizeAngle(angle)), rayMap(map){}
 
         ~MyRay(){}
 
         void Cast(){
+            // Horizontal checking
             bool foundHorizontalWall = false;
             int horHitX = 0;
             int horHitY = 0;
@@ -38,7 +43,7 @@ class MyRay{
             double firstIntersectX = NAN;
             double firstIntersectY = NAN;
 
-            if (isFacingUp) firstIntersectY = std::floor(rayPlayer.y / TILESIZE) * TILESIZE - 1;
+            if (isFacingUp) firstIntersectY = std::floor(rayPlayer.y / TILESIZE) * TILESIZE - 0.01;
             else if (isFacingDown) firstIntersectY = std::floor(rayPlayer.y / TILESIZE) * TILESIZE + TILESIZE;
 
             firstIntersectX = ((firstIntersectY - rayPlayer.y) / tan(rayAngle)) + rayPlayer.x;
@@ -54,7 +59,7 @@ class MyRay{
 
             xa = ya / tan(rayAngle);
 
-            while (nextHorX <= windowWidth && nextHorX >= 0 && nextHorY <= windowHeight && nextHorY <= 0){
+            while (nextHorX <= windowWidth && nextHorX >= 0 && nextHorY <= windowHeight && nextHorY >= 0){
                 if (rayMap.hasWallAt(nextHorX, nextHorY)){
                     foundHorizontalWall = true;
                     horHitX = nextHorX;
@@ -67,8 +72,56 @@ class MyRay{
                 }
             }
 
-            wallHitX = horHitX;
-            wallHitY = horHitY;
+            // Verical checking
+            bool foundVerticalWall = false;
+            int verHitX = 0;
+            int verHitY = 0;
+
+            if (isFacingRight) firstIntersectX = std::floor(rayPlayer.x / TILESIZE) * TILESIZE + TILESIZE;
+            else if (isFacingLeft) firstIntersectX = std::floor(rayPlayer.x / TILESIZE) * TILESIZE - 0.01;
+
+            firstIntersectY = ((rayPlayer.x - firstIntersectX) * tan(rayAngle)) + rayPlayer.y;
+
+            double nextVerX = firstIntersectX;
+            double nextVerY = firstIntersectY;
+
+            if (isFacingRight) xa = TILESIZE;
+            if (isFacingLeft) xa = -TILESIZE;
+
+            ya = xa * tan(rayAngle);
+
+            while (nextVerX <= windowWidth && nextVerX >= 0 && nextVerY <= windowHeight && nextVerY >= 0){
+                if (rayMap.hasWallAt(nextVerX, nextVerY)){
+                    foundVerticalWall= true;
+                    verHitX= nextVerX;
+                    verHitY = nextVerY;
+                    break;
+                }
+                else{
+                    nextVerX += xa;
+                    nextVerY += ya;
+                }
+            }
+
+            // Distance Calculation
+            float horDist = 0;
+            float verDist = 0;
+
+            if (foundHorizontalWall) horDist = distanceBetween(rayPlayer.x, rayPlayer.y, horHitX, horHitY);
+            else horDist = 999;
+
+            if (foundVerticalWall) verDist = distanceBetween(rayPlayer.x, rayPlayer.y, verHitX, verHitY);
+            else verDist = 999;
+
+            if (horDist < verDist){
+                wallHitX = horHitX;
+                wallHitY = horHitY;
+            }
+            else{
+                wallHitX = verHitX;
+                wallHitY = verHitY;
+            }
+
         }
 
         void Draw(){
